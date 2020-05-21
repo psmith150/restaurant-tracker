1aa6625e1dea5e49426da5ef657f208ba549f671
psmith150 <patricksmith150@gmail.com>
2020-05-20 22:44:53 -0500

    Added price rating widget. Updated CSS for star rating widget.

--- restaurant_tracker/widgets.py
+++ restaurant_tracker/widgets.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.utils.safestring import mark_safe
 
 DEFAULT_STARS = 5
+DEFAULT_PRICE = 4
 
 class StarRatingWidget(Widget):
     template_name = 'restaurant_tracker/star_rating_widget.html'
@@ -17,4 +18,17 @@ class StarRatingWidget(Widget):
     def get_context(self, name, value, attrs=None):
         context = super(StarRatingWidget, self).get_context(name, value, attrs)
         context['stars'] = range(1, self.stars+1, 1)
+        return context
+
+class PriceRatingWidget(Widget):
+    template_name = 'restaurant_tracker/price_rating_widget.html'
+
+    def __init__(self, attrs=None, prices=None):
+        super(PriceRatingWidget, self).__init__(attrs)
+
+        self.prices = prices if prices else DEFAULT_PRICE
+
+    def get_context(self, name, value, attrs=None):
+        context = super(PriceRatingWidget, self).get_context(name, value, attrs)
+        context['prices'] = range(1, self.prices+1, 1)
         return context
